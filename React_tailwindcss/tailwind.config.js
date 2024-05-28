b4cd72544b2192c766cb6db53f651bc29033fc26
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-05-28 10:40:17 -0400

    Customs colors colors in tailwindcss

--- React_tailwindcss/tailwind.config.js
+++ React_tailwindcss/tailwind.config.js
@@ -2,6 +2,19 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    // this is how you get the custom colors 
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      white: "#ffffff",
+      purple: "#3f3cbb",
+      midnight: "#121063",
+      metal: "#565584",
+      tahiti: "#3ab7bf",
+      silver: "#ecebff",
+      "bubble-gum": "#ff77e9",
+      bermuda: "#78dcca",
+    },
     extend: {},
   },
   plugins: [],

abd3d811289ee567dbb5b7b2f410fa9e267e02e3
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-05-28 12:03:25 -0400

    Tailblocks in tailwindcss

--- React_tailwindcss/src/App.jsx
+++ React_tailwindcss/src/App.jsx
@@ -3,27 +3,59 @@ import React from "react";
 const App = () => {
   return (
     // using container and mx-auto will put it in the center
-    <div className="container mx-auto border-2 ">
-      <div className=" p-10 font-bold underline text-blue-500">hello</div>
 
-      {/* min and max width are good for making responsive sites */}
-      <div className="container mx-auto border-2 border-red-900 min-w-1 max-w-[300px]">
-        width property
-      </div>
+    <div>
+      <header class="text-gray-600 body-font">
+        <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+          <a class="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              class="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full"
+              viewBox="0 0 24 24"
+            >
+              <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
+            </svg>
+            <span class="ml-3 text-xl">Tailblocks</span>
+          </a>
+          <nav class="md:ml-auto md:mr-auto flex flex-wrap items-center text-base justify-center">
+            <a class="mr-5 hover:text-gray-900">First Link</a>
+            <a class="mr-5 hover:text-gray-900">Second Link</a>
+            <a class="mr-5 hover:text-gray-900">Third Link</a>
+            <a class="mr-5 hover:text-gray-900">Fourth Link</a>
+          </nav>
+          <button class="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">
+            Button
+            <svg
+              fill="none"
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              class="w-4 h-4 ml-1"
+              viewBox="0 0 24 24"
+            >
+              <path d="M5 12h14M12 5l7 7-7 7"></path>
+            </svg>
+          </button>
+        </div>
+      </header>
+      <div className="container mx-auto border-2 ">
+        <div className=" p-10 font-bold underline text-blue-500">hello</div>
 
-      <br />
-      {/* this will background color will change based on the screen size */}
-      <div className="sm:bg-red-500 md:bg-blue-400 lg:bg-green-500 xl:bg-gray-500 2x:bg-black">
-        <h1>will change</h1>
-      </div>
+        {/* min and max width are good for making responsive sites */}
+        <div className="container mx-auto border-2 border-red-900 min-w-1 max-w-[300px]">
+          width property
+        </div>
 
-      <div>
         <br />
-        <div className="sm:bg-green-400 sm:flex">
-          <div className="border-2 border-red-500"></div>
-          <div className="border-2 border-red-500"></div>
-          <div className="border-2 border-red-500"></div>
-          <div className="border-2 border-red-500"></div>
+        {/* this will background color will change based on the screen size */}
+        <div className="sm:bg-red-500 md:bg-blue-400 lg:bg-green-500 xl:bg-gray-500 2x:bg-black">
+          <h1>will change</h1>
         </div>
       </div>
     </div>
